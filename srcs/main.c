04caca01baad12febfb7ce8eb043a084cbc7702b
takira <ca.fie.0220@gmail.com>
2023-03-06 16:16:05 +0900

    [#2]implement put square

--- srcs/main.c
+++ srcs/main.c
@@ -6,32 +6,68 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 12:14:24 by takira            #+#    #+#             */
-/*   Updated: 2023/03/06 15:35:09 by takira           ###   ########.fr       */
+/*   Updated: 2023/03/06 16:12:02 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	put_square(int s_x, int s_y, int g_x, int g_y, int color, t_data *data)
+{
+	for (int x=s_x; x<=g_x; x++)
+		for (int y=s_y; y<=g_y; y++)
+		{
+			my_mlx_pixel_put(data, x, y, color);
+		}
+}
+
 int	main(void)
 {
-	int		x;
-	int		y;
-	int		r, g, b;
+//	int		x;
+//	int		y;
+//	int		r, g, b;
+	t_data	data;
 
-	y = HEIGHT;
-	while (y > 0)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			r = 256 * x / (WIDTH - 1);
-			g = 256 * y / (HEIGHT - 1);
-			b = 256 * 0.25;
-			printf("%d, %d, %d\n", r, g, b);
-			x++;
-		}
-		y--;
-	}
+	data.mlx = mlx_init();
+	if (!data.mlx)
+		return (EXIT_FAILURE);
+	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, TITLE);
+	if (!data.win)
+		return (EXIT_FAILURE);
+	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	if (!data.img)
+		return (EXIT_FAILURE);
+
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	put_square(5, 5, 100, 100,0x00FFFFFF, &data);
+	put_square(100, 100, 200, 200,0x0000FF00, &data);
+	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+
+	mlx_loop(data.mlx);
+
+//
+//	y = HEIGHT;
+//	while (y > 0)
+//	{
+//		x = 0;
+//		while (x < WIDTH)
+//		{
+//			r = 256 * x / (WIDTH - 1);
+//			g = 256 * y / (HEIGHT - 1);
+//			b = 256 * 0.25;
+//			printf("%d, %d, %d\n", r, g, b);
+//			x++;
+//		}
+//		y--;
+//	}
 
 	return (0);
 }
--- srcs/minirt.h
+++ srcs/minirt.h
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 22:42:42 by takira            #+#    #+#             */
-/*   Updated: 2023/03/05 22:43:22 by takira           ###   ########.fr       */
+/*   Updated: 2023/03/06 16:08:12 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,25 @@
 # define MINIRT_H
 
 # include <stdio.h>
+# include <stdlib.h>
 
-# define WIDTH	256
-# define HEIGHT	256
+# include "../minilibx-linux/mlx.h"
+
+# define WIDTH	500
+# define HEIGHT	500
+# define TITLE	"miniRT"
+
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
 
 
 #endif //MINIRT_H
