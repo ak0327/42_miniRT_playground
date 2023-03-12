5aa4f3e8c65f9757967ba7e719bc17186425db33
takira <ca.fie.0220@gmail.com>
2023-03-12 20:23:29 +0900

    [#3]Refactor func in ray.c

--- srcs/ray.c
+++ srcs/ray.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 17:26:30 by takira            #+#    #+#             */
-/*   Updated: 2023/03/12 17:28:58 by takira           ###   ########.fr       */
+/*   Updated: 2023/03/12 20:23:11 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,115 @@ int	raytrace(const t_scene *scene, const t_ray *eye_ray, t_colorf *out_col)
 
 }
 
-int intersection_test(const t_shape *shape, const t_ray * ray, t_intersection_point * out_intp)
+int	intersection_with_sphere(const t_shape *shape, const t_ray *ray, t_intersection_point *out_intp)
 {
+	const t_sphere	*sph = &shape->data.sphere;
+	t_vector		pe_pc;
+	float			A, B, C, D;
+	float			t;
+	t_vector		td;
 
+	pe_pc = sub(&ray->start, &sph->center);
+	A = dot(&ray->direction, &ray->direction);
+	B = 2 * dot(&ray->direction, &pe_pc);
+	C = dot(&pe_pc, &pe_pc) - SQR(sph->radius);
+
+	D = SQR(B) - 4 * A * C;
+
+	t = -1.0f;
+	if (D == 0)
+		t = -B / (2 * A);
+	else if (D > 0)
+	{
+		float t1 = (float) (-B + sqrtf(D)) / (2 * A);
+		float t2 = (float) (-B - sqrtf(D)) / (2 * A);
+		if (t1 > 0) t = t1;
+		if (t2 > 0 && t2 < t) t = t2;
+	}
+
+	if (t <= 0)
+		return (0);
+	if (!out_intp)
+		return (0);
+
+	out_intp->distance = t;
+	td = mult(t, &ray->direction);
+	out_intp->position = add(&ray->start, &td);
+	out_intp->normal = sub(&out_intp->position, &sph->center);
+	normalize(&out_intp->normal);
+	return (1);
+}
+
+int	intersection_with_plane(const t_shape *shape, const t_ray *ray, t_intersection_point *out_intp)
+{
+	const t_plane	*pln = &shape->data.plane;
+	float 			dn_dot = dot(&ray->direction, &pln->normal);
+	t_vector		s_p;
+	float			t;
+	t_vector		td;
+
+	if (dn_dot == 0)
+		return (0);
+
+	s_p = sub(&ray->start, &pln->position);
+	t = -dot(&s_p, &pln->normal) / dn_dot;
+
+	if (t <= 0)
+		return (0);
+
+	if (!out_intp)
+		return (0);
+
+	out_intp->distance = t;
+	td = mult(t, &ray->direction);
+	out_intp->position = add(&ray->start, &td);
+	out_intp->normal = pln->normal;
+	return (1);
+}
+
+int intersection_test(const t_shape *shape, const t_ray *ray, t_intersection_point *out_intp)
+{
+	if ( shape->type == ST_SPHERE )
+		return (intersection_with_sphere(shape, ray, out_intp));
+	else if ( shape->type == ST_PLANE )
+		return (intersection_with_plane(shape, ray, out_intp));
+	return (0);
 }
 
 int get_nearest_shape(const t_scene *scene, const t_ray *ray, float max_dist, int exit_once_found,
 					  t_shape **out_shape, t_intersection_point *out_intp)
 {
+	size_t	i;
+	t_shape *nearest_shape = NULL;
+	t_intersection_point nearest_intp;
+	nearest_intp.distance = max_dist;
 
+	for(i = 0; i < scene->num_shapes; ++i)
+	{
+		int res;
+		t_intersection_point intp;
+
+		res = intersection_test(&scene->shapes[i], ray, &intp);
+
+		if ( res && intp.distance < nearest_intp.distance )
+		{
+			nearest_shape = &scene->shapes[i];
+			nearest_intp = intp;
+			if ( exit_once_found ) break;
+		}
+	}/* for */
+
+	if ( nearest_shape )
+	{
+		if ( out_shape )
+			*out_shape = nearest_shape;
+		if ( out_intp )
+			*out_intp = nearest_intp;
+
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
