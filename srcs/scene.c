fb1db4c1f7a2bf0929a8aecab7aa56a5458459f2
takira <ca.fie.0220@gmail.com>
2023-03-22 01:50:01 +0900

    push now working

--- srcs/scene.c
+++ srcs/scene.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 17:28:16 by takira            #+#    #+#             */
-/*   Updated: 2023/03/21 18:33:23 by takira           ###   ########.fr       */
+/*   Updated: 2023/03/21 19:40:06 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -219,7 +219,7 @@ void scene_setting(t_scene *scene)
 
 	init_material(&scene->shapes[2].material,
 				  0.01f, 0.01f, 0.01f, 	/* 環境光係数(RGB)   */
-				  0.0f, 0.0f, 1.0f,		/* 拡散反射係数(RGB) */
+				  0.0f, 0.0f, 1.0f,			/* 拡散反射係数(RGB) */
 				  0.30f, 0.30f, 0.30f,		/* 鏡面反射率(RGB)   */
 				  8.0f,
 				  MT_DEFAULT,
@@ -235,7 +235,7 @@ void scene_setting(t_scene *scene)
 
 	init_material(&scene->shapes[3].material,
 				  0.01f, 0.01f, 0.01f, 	/* 環境光係数(RGB)   */
-				  0.0f, 1.0f, 0.0f,		/* 拡散反射係数(RGB) */
+				  0.0f, 1.0f, 0.0f,			/* 拡散反射係数(RGB) */
 				  0.30f, 0.30f, 0.30f,		/* 鏡面反射率(RGB)   */
 				  8.0f,
 				  MT_DEFAULT,
