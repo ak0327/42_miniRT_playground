fb17c2ba3a6601f9ceae7ad7ac876d4c71902fa2
takira <ca.fie.0220@gmail.com>
2023-04-07 12:25:30 +0900

    [#9] Fix: update trans matrix for put img on cylinder

--- srcs/bump_mapping.c
+++ srcs/bump_mapping.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 10:13:41 by takira            #+#    #+#             */
-/*   Updated: 2023/04/07 10:40:14 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/07 12:24:53 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_vector	get_bump_normal(const t_scene *scene, const t_ray *eye_ray,
 							t_intersection_point intp, t_shape *shape, t_img img)
 {
-	t_vector	bump_n;
+	t_vector	bump_normal_local;
 	int			r, g, b;
 	size_t		row, col, idx;
 	int			put_size;
@@ -24,7 +24,7 @@ t_vector	get_bump_normal(const t_scene *scene, const t_ray *eye_ray,
 	t_vector	pos_local;
 	float		radius, theta, phi;
 
-	bump_n = intp.normal;
+	bump_normal_local = intp.normal;
 
 	if (shape->type == ST_PLANE)
 	{
@@ -41,11 +41,11 @@ t_vector	get_bump_normal(const t_scene *scene, const t_ray *eye_ray,
 		r = img.data[idx++];
 		g = img.data[idx++];
 		b = img.data[idx];
-		bump_n.x = ((float)r - (256.0f/2.0f)) / (256.0f/2.0f);
-		bump_n.z = ((float)g - (256.0f/2.0f)) / (256.0f/2.0f);
-		bump_n.y = ((float)b - (256.0f/2.0f)) / (256.0f/2.0f);
-		normalize(&bump_n);
-		return (bump_n);
+		bump_normal_local.x = ((float)r - (256.0f / 2.0f)) / (256.0f / 2.0f);
+		bump_normal_local.z = ((float)g - (256.0f / 2.0f)) / (256.0f / 2.0f);
+		bump_normal_local.y = ((float)b - (256.0f / 2.0f)) / (256.0f / 2.0f);
+		normalize(&bump_normal_local);
+		return (bump_normal_local);
 	}
 	if (shape->type == ST_SPHERE)
 	{
@@ -73,13 +73,13 @@ t_vector	get_bump_normal(const t_scene *scene, const t_ray *eye_ray,
 		r = img.data[idx++];
 		g = img.data[idx++];
 		b = img.data[idx];
-		bump_n.x = ((float)r - (256.0f/2.0f)) / (256.0f/2.0f);
-		bump_n.z = ((float)g - (256.0f/2.0f)) / (256.0f/2.0f);
-		bump_n.y = ((float)b - (256.0f/2.0f)) / (256.0f/2.0f);
-		normalize(&bump_n);
+		bump_normal_local.x = ((float)r - (256.0f / 2.0f)) / (256.0f / 2.0f);
+		bump_normal_local.z = ((float)g - (256.0f / 2.0f)) / (256.0f / 2.0f);
+		bump_normal_local.y = ((float)b - (256.0f / 2.0f)) / (256.0f / 2.0f);
+		normalize(&bump_normal_local);
 
 		/* (u,v,w)->(x,y,z) */
-		t_vector	bump_world_n;
+		t_vector	bump_normal_world;
 		t_vector	eu, ev, ew;
 		t_vector	ey;
 		t_matrix	Tr_matrix;
@@ -104,13 +104,13 @@ t_vector	get_bump_normal(const t_scene *scene, const t_ray *eye_ray,
 
 		Tr_matrix = set_matrix(eu, ew, ev);
 		Tr_matrix = transpose_matrix(Tr_matrix);
-		bump_world_n = Mv(Tr_matrix, bump_n);
-		normalize(&bump_world_n);
-		return (bump_world_n);
+		bump_normal_world = Mv(Tr_matrix, bump_normal_local);
+		normalize(&bump_normal_world);
+		return (bump_normal_world);
 	}
 
 
-	return (bump_n);
+	return (bump_normal_local);
 }
 
 
@@ -123,7 +123,7 @@ t_colorf	get_img_color(const t_scene *scene, const t_ray *eye_ray,
 	size_t		row, col, idx;
 	int			put_size = 1;
 	int			img_size;
-	float		u, v;
+	float		u, v, w;
 	float		radius, theta, phi;
 
 	SET_COLOR(color, 0.0f, 0.0f, 0.0f);
@@ -173,71 +173,46 @@ t_colorf	get_img_color(const t_scene *scene, const t_ray *eye_ray,
 	if (shape->type == ST_CYLINDER)
  	{
 		/* u,v */
-		t_vector	hi, d;
-		float		uu, vv;
-		t_vector	u_vec, v_vec;
+		float		uu, vv, ww;
 		t_vector	eu, ev, ew;
-		t_vector	ey;
+		t_vector	ex, ey;
 		t_vector	pos_uv;
-		float		l;
 		t_matrix	M;
 
 		pos_local = sub(&intp.position, &shape->data.cylinder.position);
-		hi = mult(dot(&pos_local, &shape->data.cylinder.normal), &shape->data.cylinder.normal);
-		d = shape->data.cylinder.normal;
 
-//		SET_VECTOR(ey, 0.0f, 1.0f, 0.0f);
-//		ew = shape->data.cylinder.normal;
-//		eu = cross(&ey, &ew);
-//		ev = cross(&eu, &ew);
-//
-//		if (ew.x == ey.x && ew.y == ey.y && ew.z == ey.z)
-//		{
-//			SET_VECTOR(eu, 1.0f, 0.0f, 0.0f);
-//			SET_VECTOR(ev, 0.0f, 0.0f, 1.0f);
-//		}
-//		if (ew.x == ey.x && ew.y == -ey.y && ew.z == ey.z)
-//		{
-//			SET_VECTOR(eu, -1.0f, 0.0f, 0.0f);
-//			SET_VECTOR(ev, 0.0f, 0.0f, -1.0f);
-//		}
-//
-//		M = set_matrix(eu, ew, ev);	// (i,j,k)->(u,w,v)への変換matrix
-////		M = transpose_matrix(M);				// (u,w,v)->(i,j,k)への変換matrix
-//
-//		pos_uv = Mv(M, pos_local);
-//
-//		u = pos_uv.x;
-//		v = pos_uv.z;
-//		l = sqrtf(SQR(u) + SQR(v));
-//		if (l == 0)
-//			theta = 0;
-//		else
-//			theta = acosf(u / l);
-//
-//		uu = theta / (float)M_PI;						// 0 <= uu <= 1
-//		vv = pos_uv.y / shape->data.cylinder.height;
+		SET_VECTOR(ex, 1.0f, 0.0f, 0.0f);
+		SET_VECTOR(ey, 0.0f, 1.0f, 0.0f);
 
-//		printf("(u,v)=(%f,%f), (uu,vv)=(%f,%f), theta:%f\n", u, v, uu, vv, theta * 180 / (float)M_PI);
+		ew = shape->data.cylinder.normal;
+		ev = cross(&ex, &ew);
+		normalize(&ev);
+		eu = cross(&ew, &ev);
+		normalize(&eu);
 
+		if (ew.x == ex.x && ew.y == ex.y && ew.z == ex.z)
+		{
+			SET_VECTOR(eu, 0.0f, -1.0f, 0.0f);
+			SET_VECTOR(ev, 0.0f, 0.0f, 1.0f);
+		}
+		if (ew.x == -ex.x && ew.y == ex.y && ew.z == ex.z)
+		{
+			SET_VECTOR(eu, 0.0f, 1.0f, 0.0f);
+			SET_VECTOR(ev, 0.0f, 0.0f, 1.0f);
+		}
 
-		u_vec.x = d.y / sqrtf(SQR(d.x) + SQR(d.y));
-		u_vec.y = -d.x / sqrtf(SQR(d.x) + SQR(d.y));
-		u_vec.z = 0;
-		normalize(&u_vec);
+		M = set_matrix(eu, ew, ev);	// (i,j,k)->(u,w,v)への変換matrix
+//		M = transpose_matrix(M);				// (u,w,v)->(i,j,k)への変換matrix
 
-		v_vec = cross(&u_vec, &d);
-		normalize(&v_vec);
+		pos_uv = Mv(M, pos_local);
 
-		u = dot(&u_vec, &pos_local);
-		v = dot(&v_vec, &pos_local);
+		u = pos_uv.x; //このuは結局xになっているのでは？
+		v = pos_uv.z;
+		w = pos_uv.y;
 
-		theta = atan2f(v, u);
-//		uu = 1.0f - (theta / (2.0f * (float)M_PI) + 0.5f);
-		uu = theta * 2 / (2.0f * (float)M_PI);
-		vv = norm(&hi) / shape->data.cylinder.height;
-
-		//		printf("(u,v)=(%f,%f), (uu,vv)=(%f,%f)\n", u, v, uu, vv);
+		theta = acosf(u / shape->data.cylinder.radius);
+		uu = theta / (float)M_PI;
+		vv = w / shape->data.cylinder.height;
 
 		uu *= (float)img.width;
 		vv *= -(float)img.height;
@@ -251,8 +226,6 @@ t_colorf	get_img_color(const t_scene *scene, const t_ray *eye_ray,
 		b = img.data[idx];
 		SET_COLOR(color, (float)r/255.0f, (float)g/255.0f, (float)b/255.0f);
 		return (color);
-
-
 	}
 	return (color);
 }
