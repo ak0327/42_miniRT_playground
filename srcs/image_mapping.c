f0b817600a9ca2870115d642ac2be2402ddf9dd3
takira <ca.fie.0220@gmail.com>
2023-04-08 23:17:57 +0900

    [#15]Change: Change image_on_plane function for use matrix

--- srcs/image_mapping.c
+++ srcs/image_mapping.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 22:45:27 by takira            #+#    #+#             */
-/*   Updated: 2023/04/08 22:50:31 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/08 23:17:05 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,28 @@
 
 static t_colorf	get_image_color_on_plane(t_intersection_point intp, t_shape *shape, t_img img)
 {
-	t_colorf	color;
-	t_vector	pos_local;
-	int			r, g, b;
-	size_t		row, col, idx;
-	int			put_size = 1;
-	int			img_size;
-	float		u, v, w;
-	float		radius, theta, phi;
+	t_colorf		color;
+	t_vector		pos_local;
+	int				r, g, b;
+	size_t			row, col, idx;
+	int				put_size = 1;
+	int				img_size;
+	t_matrix		Tr_matrix;
+	t_texture_map	pattern_map;
 
-	float		uu, vv;
-	t_vector	pos_uv;
-	t_matrix	Tr;
+	pos_local = sub(&intp.position, &shape->data.plane.position);
+	Tr_matrix = get_tr_matrix_world2obj_vup(shape->data.plane.normal);
+	pattern_map = get_planar_map(pos_local, Tr_matrix);
 
-	t_vector	hi, d;
-	float		h;
+	row = (((int)pattern_map.u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = ((-(int)pattern_map.v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
+	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
+	r = img.data[idx++];
+	g = img.data[idx++];
+	b = img.data[idx];
+	SET_COLOR(color, (float)r/255.0f, (float)g/255.0f, (float)b/255.0f);
+	return (color);
 }
 
 static t_colorf	get_image_color_on_sphere(t_intersection_point intp, t_shape *shape, t_img img)
@@ -40,15 +46,8 @@ static t_colorf	get_image_color_on_sphere(t_intersection_point intp, t_shape *sh
 	size_t		row, col, idx;
 	int			put_size = 1;
 	int			img_size;
-	float		u, v, w;
+	float		u, v;
 	float		radius, theta, phi;
-
-	float		uu, vv;
-	t_vector	pos_uv;
-	t_matrix	Tr;
-
-	t_vector	hi, d;
-	float		h;
 
 	pos_local = sub(&intp.position, &shape->data.sphere.center);
 	radius = norm(&pos_local);
@@ -74,8 +73,6 @@ static t_colorf	get_image_color_on_sphere(t_intersection_point intp, t_shape *sh
 	b = img.data[idx];
 	SET_COLOR(color, (float)r/255.0f, (float)g/255.0f, (float)b/255.0f);
 	return (color);
-
-
 }
 
 static t_colorf	get_image_color_on_cylinder(t_intersection_point intp, t_shape *shape, t_img img)
@@ -86,38 +83,27 @@ static t_colorf	get_image_color_on_cylinder(t_intersection_point intp, t_shape *
 	size_t		row, col, idx;
 	int			put_size = 1;
 	int			img_size;
-	float		u, v, w;
-	float		radius, theta, phi;
-
-	float		uu, vv;
+	float		radius, theta;
+	float		u, v;
 	t_vector	pos_uv;
-	t_matrix	Tr;
-
-	t_vector	hi, d;
-	float		h;
+	t_matrix	Tr_matrix;
 
 	pos_local = sub(&intp.position, &shape->data.cylinder.position);
-	hi = mult(dot(&pos_local, &shape->data.cylinder.normal), &shape->data.cylinder.normal);
-	d = shape->data.cylinder.normal;
-	h = shape->data.cylinder.height;
 	radius = shape->data.cylinder.radius;
 
-	Tr = get_tr_matrix_world2obj(d);
+	Tr_matrix = get_tr_matrix_world2obj(shape->data.cylinder.normal);
 
-	pos_uv = mul_matrix_vec(Tr, pos_local);	// pos(x,y,z)->pos(u,v,w)
+	pos_uv = mul_matrix_vec(Tr_matrix, pos_local);	// pos(x,y,z)->pos(u,v,w)
 
-	u = pos_uv.x;
-	w = pos_uv.y;
+	theta = acosf(pos_uv.x / radius);				// 0 <= theta <= pi
+	u = theta / (float)M_PI;						// 0 <= u <= 1
+	v = pos_uv.y / shape->data.cylinder.height;		// 0 <= v <= 1
 
-	theta = acosf(u / radius);		// 0 <= theta <= pi
-	uu = theta / (float)M_PI;		// 0 <= uu <= 1
-	vv = w / h;						// 0 <= vv <= 1
+	u *= -(float)img.width;
+	v *= -(float)img.height;
 
-	uu *= -(float)img.width;
-	vv *= -(float)img.height;
-
-	row = (((int)uu % img.width) + img.width) % img.width;		// 0 <= row <= img.width
-	col = (((int)vv % img.height) + img.height) % img.height;	// 0 <= col <= img.height
+	row = (((int)u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = (((int)v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
 	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
 	r = img.data[idx++];
@@ -125,8 +111,6 @@ static t_colorf	get_image_color_on_cylinder(t_intersection_point intp, t_shape *
 	b = img.data[idx];
 	SET_COLOR(color, (float)r/255.0f, (float)g/255.0f, (float)b/255.0f);
 	return (color);
-
-
 }
 
 static t_colorf	get_image_color_on_corn(t_intersection_point intp, t_shape *shape, t_img img)
@@ -137,38 +121,30 @@ static t_colorf	get_image_color_on_corn(t_intersection_point intp, t_shape *shap
 	size_t		row, col, idx;
 	int			put_size = 1;
 	int			img_size;
-	float		u, v, w;
-	float		radius, theta, phi;
+	float		u, v;
+	float		radius, theta;
 
-	float		uu, vv;
 	t_vector	pos_uv;
-	t_matrix	Tr;
+	t_matrix	Tr_matrix;
 
-	t_vector	hi, d;
-	float		h;
+	t_vector	hi;
 
 	pos_local = sub(&intp.position, &shape->data.corn.position);
 	hi = mult(dot(&pos_local, &shape->data.corn.normal), &shape->data.corn.normal);
-	d = shape->data.corn.normal;
-	h = shape->data.corn.height;
-	radius = shape->data.corn.radius * norm(&hi) / h;
+	radius = shape->data.corn.radius * norm(&hi) / shape->data.corn.height;
 
-	Tr = get_tr_matrix_world2obj(d);
+	Tr_matrix = get_tr_matrix_world2obj(shape->data.corn.normal);
+	pos_uv = mul_matrix_vec(Tr_matrix, pos_local);	// pos(x,y,z)->pos(u,v,w)
 
-	pos_uv = mul_matrix_vec(Tr, pos_local);	// pos(x,y,z)->pos(u,v,w)
+	theta = acosf(pos_uv.x / radius);				// 0 <= theta <= pi
+	u = theta / (float)M_PI;						// 0 <= u <= 1
+	v = pos_uv.y / shape->data.corn.height;			// 0 <= v <= 1
 
-	u = pos_uv.x;
-	w = pos_uv.y;
+	u *= -(float)img.width;
+	v *= -(float)img.height;
 
-	theta = acosf(u / radius);		// 0 <= theta <= pi
-	uu = theta / (float)M_PI;		// 0 <= uu <= 1
-	vv = w / h;						// 0 <= vv <= 1
-
-	uu *= -(float)img.width;
-	vv *= -(float)img.height;
-
-	row = (((int)uu % img.width) + img.width) % img.width;		// 0 <= row <= img.width
-	col = (((int)vv % img.height) + img.height) % img.height;	// 0 <= col <= img.height
+	row = (((int)u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = (((int)v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
 	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
 	r = img.data[idx++];
