79b7d93d37cb37c060cb2ceccbc34ae2ac30e55e
takira <ca.fie.0220@gmail.com>
2023-04-09 19:08:41 +0900

    [#15]極座標系の方位角を0<=phi<=2piまで対応するよう変更、合わせてget_image_color_on* にmap関数を使用

--- srcs/get_img_color.c
+++ srcs/get_img_color.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 22:45:27 by takira            #+#    #+#             */
-/*   Updated: 2023/04/09 14:43:55 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/09 19:07:36 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ static t_colorf	get_image_color_on_plane(t_intersection_point intp, t_shape *sha
 	t_vector		pos_local;
 	int				r, g, b;
 	size_t			row, col, idx;
-	int				put_size = 1;
-	int				img_size;
+	int				img_size = 1;
 	t_matrix		Tr_matrix;
-	t_texture_map	pattern_map;
+	t_texture_map	map;
 
 	pos_local = sub(&intp.position, &shape->data.plane.position);
 	Tr_matrix = get_tr_matrix_world2obj_yup(shape->data.plane.normal);
-	pattern_map = get_planar_map(pos_local, Tr_matrix);
+	map = get_planar_map(pos_local, Tr_matrix);
 
-	row = ((((int)pattern_map.u * put_size) % img.width) + img.width) % img.width;		// 0 <= row <= img.width
-	col = (((-(int)pattern_map.v * put_size) % img.height) + img.height) % img.height;	// 0 <= col <= img.height
+	row = ((((int)map.u * img_size) % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = (((-(int)map.v * img_size) % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
 	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
 	r = img.data[idx++];
@@ -40,33 +39,21 @@ static t_colorf	get_image_color_on_plane(t_intersection_point intp, t_shape *sha
 
 static t_colorf	get_image_color_on_sphere(t_intersection_point intp, t_shape *shape, t_img img)
 {
-	t_colorf	color;
-	t_vector	pos_local;
-	int			r, g, b;
-	size_t		row, col, idx;
-	int			put_size = 1;
-	int			img_size;
-	float		u, v;
-	float		radius, theta, phi;
-	int			frequency = 1;
+	t_colorf		color;
+	t_vector		pos_local;
+	int				r, g, b;
+	size_t			row, col, idx;
+	int				frequency = 1;
+	t_texture_map	map;
 
 	pos_local = sub(&intp.position, &shape->data.sphere.center);
-	radius = norm(&pos_local);
-	theta = acosf(pos_local.y / radius);
 
-	if (radius * sinf(theta) == 0.0f)
-		phi = 0.0f;
-	else
-		phi = acosf(pos_local.x / (radius * sinf(theta)));
+	map = get_spherical_map(pos_local);
+	map.u *= -(float)img.width * (float)frequency;
+	map.v *= (float)img.height * (float)frequency;
 
-	u = phi / (float)M_PI;		// 0 <= u <= 1
-	v = theta / (float)M_PI;	// 0 <= v <= 1
-
-	u *= -(float)img.width * (float)frequency;
-	v *= (float)img.height * (float)frequency;
-
-	row = (((int)u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
-	col = (((int)v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
+	row = (((int)map.u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = (((int)map.v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
 	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
 	r = img.data[idx++];
@@ -78,34 +65,23 @@ static t_colorf	get_image_color_on_sphere(t_intersection_point intp, t_shape *sh
 
 static t_colorf	get_image_color_on_cylinder(t_intersection_point intp, t_shape *shape, t_img img)
 {
-	t_colorf	color;
-	t_vector	pos_local;
-	int			r, g, b;
-	size_t		row, col, idx;
-	int			put_size = 1;
-	int			img_size;
-	float		radius, theta;
-	float		u, v;
-	t_vector	pos_uv;
-	t_matrix	Tr_matrix;
-	int			frequency = 1;
+	t_colorf		color;
+	t_vector		pos_local;
+	int				r, g, b;
+	size_t			row, col, idx;
+	t_matrix		Tr_matrix;
+	int				frequency = 1;
+	t_texture_map	map;
 
 	pos_local = sub(&intp.position, &shape->data.cylinder.position);
-	radius = shape->data.cylinder.radius;
-
 	Tr_matrix = get_tr_matrix_world2obj_zup(shape->data.cylinder.normal);
 
-	pos_uv = mul_matrix_vec(Tr_matrix, pos_local);	// pos(x,y,z)->pos(u,v,w)
+	map = get_cylindrical_map(pos_local, Tr_matrix, shape->data.cylinder.height);
+	map.u *= -(float)img.width * (float)frequency;
+	map.v *= -(float)img.height * (float)frequency;
 
-	theta = acosf(pos_uv.x / radius);				// 0 <= theta <= pi
-	u = theta / (float)M_PI;						// 0 <= u <= 1
-	v = pos_uv.y / shape->data.cylinder.height;		// 0 <= v <= 1
-
-	u *= -(float)img.width * (float)frequency;
-	v *= -(float)img.height * (float)frequency;
-
-	row = (((int)u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
-	col = (((int)v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
+	row = (((int)map.u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = (((int)map.v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
 	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
 	r = img.data[idx++];
@@ -117,37 +93,24 @@ static t_colorf	get_image_color_on_cylinder(t_intersection_point intp, t_shape *
 
 static t_colorf	get_image_color_on_corn(t_intersection_point intp, t_shape *shape, t_img img)
 {
-	t_colorf	color;
-	t_vector	pos_local;
-	int			r, g, b;
-	size_t		row, col, idx;
-	int			put_size = 1;
-	int			img_size;
-	float		u, v;
-	float		radius, theta;
-
-	t_vector	pos_uv;
-	t_matrix	Tr_matrix;
-
-	t_vector	hi;
-	int			frequency = 1;
+	t_colorf		color;
+	t_vector		pos_local;
+	int				r, g, b;
+	size_t			row, col, idx;
+	t_matrix		Tr_matrix;
+	t_texture_map	map;
+	int				frequency = 1;
 
 	pos_local = sub(&intp.position, &shape->data.corn.position);
-	hi = mult(dot(&pos_local, &shape->data.corn.normal), &shape->data.corn.normal);
-	radius = shape->data.corn.radius * norm(&hi) / shape->data.corn.height;
 
 	Tr_matrix = get_tr_matrix_world2obj_zup(shape->data.corn.normal);
-	pos_uv = mul_matrix_vec(Tr_matrix, pos_local);	// pos(x,y,z)->pos(u,v,w)
 
-	theta = acosf(pos_uv.x / radius);				// 0 <= theta <= pi
-	u = theta / (float)M_PI;						// 0 <= u <= 1
-	v = pos_uv.y / shape->data.corn.height;			// 0 <= v <= 1
+	map = get_conical_map(pos_local, Tr_matrix, shape->data.corn.height);
+	map.u *= -(float)img.width * (float)frequency;
+	map.v *= -(float)img.height * (float)frequency;
 
-	u *= -(float)img.width * (float)frequency;
-	v *= -(float)img.height * (float)frequency;
-
-	row = (((int)u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
-	col = (((int)v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
+	row = (((int)map.u % img.width) + img.width) % img.width;		// 0 <= row <= img.width
+	col = (((int)map.v % img.height) + img.height) % img.height;	// 0 <= col <= img.height
 
 	idx = ((col * img.width + row) * 3) % (img.width * img.height * 3);
 	r = img.data[idx++];
--- srcs/get_texture_map.c
+++ srcs/get_texture_map.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 14:02:49 by takira            #+#    #+#             */
-/*   Updated: 2023/04/09 14:46:02 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/09 19:05:43 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ t_texture_map	get_cylindrical_map(t_vector pos_local, t_matrix Tr, float h)
 {
 	t_vector		pos_uv;
 	t_texture_map	map;
-	float			theta;
+	float			phi;
 
 	pos_uv = mul_matrix_vec(Tr, pos_local);	// pos(x,y,z)->pos(u,v,w)
-	theta = atan2f(pos_uv.z, pos_uv.x);
-	map.u = 1.0f - (theta / (2.0f * (float)M_PI) + 0.5f);
+	phi = atan2f(pos_uv.z, pos_uv.x);
+	map.u = (phi + (float)M_PI) / (2.0f * (float)M_PI);
 	map.v = pos_uv.y / h;
 	return (map);
 }
@@ -29,11 +29,11 @@ t_texture_map	get_conical_map(t_vector pos_local, t_matrix Tr, float h)
 {
 	t_vector		pos_uv;
 	t_texture_map	map;
-	float			theta;
+	float			phi;
 
 	pos_uv = mul_matrix_vec(Tr, pos_local);	// pos(x,y,z)->pos(u,v,w)
-	theta = atan2f(pos_uv.z, pos_uv.x);
-	map.u = 1.0f - (theta / (2.0f * (float)M_PI) + 0.5f);
+	phi = atan2f(pos_uv.z, pos_uv.x);
+	map.u = (phi + (float)M_PI) / (2.0f * (float)M_PI);
 	map.v = pos_uv.y / h;
 	return (map);
 }
@@ -58,13 +58,9 @@ t_texture_map	get_spherical_map(t_vector pos_local)
 
 	radius = norm(&pos_local);
 	theta = acosf(pos_local.y / radius);
+	phi = atan2f(pos_local.z, pos_local.x);
 
-	if (radius * sinf(theta) == 0.0f)
-		phi = 0.0f;
-	else
-		phi = acosf(pos_local.x / (radius * sinf(theta)));
-
-	map.u = phi / (float)M_PI;		// 0 <= fu <= 1
+	map.u = (phi + (float)M_PI) / (2.0f * (float)M_PI);		// 0 <= fu <= 1
 	map.v = theta / (float)M_PI;	// 0 <= fv <= 1
 	return (map);
 }
