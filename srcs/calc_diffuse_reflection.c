014ea518566511722f2b0dddd8834ebf57f98642
takira <ca.fie.0220@gmail.com>
2023-04-10 14:20:18 +0900

    [#16]calc_diffuse_reflection()を関数分割した

--- srcs/calc_diffuse_reflection.c
+++ srcs/calc_diffuse_reflection.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 11:12:18 by takira            #+#    #+#             */
-/*   Updated: 2023/04/10 13:56:42 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/10 14:18:11 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int is_obj_exists_between_light_and_eye(const t_scene *scene, t_vector dir_pos2l
 	/* shadow_rayを計算 */
 	shadow_ray.start = vec_calc(1.0f, &intp.position, EPSILON, &dir_pos2light);
 	shadow_ray.direction = dir_pos2light;
-	if (light->type == LT_POINT)
+	if (light->type == LT_POINT || light->type == LT_SPOT)
 	{
 		vec_pi_to_light = sub(&light->position, &intp.position);
 		dist = norm(&vec_pi_to_light) - EPSILON;
@@ -45,14 +45,99 @@ int is_obj_exists_between_light_and_eye(const t_scene *scene, t_vector dir_pos2l
 	return (shadow_intersect_result);
 }
 
+t_colorf	get_checker_reflection_color(t_shape *shape, t_intersection_point intp, float nl_dot)
+{
+	t_colorf	color, checker_col;
+
+	SET_COLOR(color, 0.0f, 0.0f, 0.0f);
+
+	/* checker */
+	if (!shape->material.is_checker)
+		return (color);
+
+	checker_col = get_checker_color(intp, shape);
+	color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&checker_col);
+	return (color);
+}
+
+t_colorf	get_image_reflection_color(t_shape *shape, t_intersection_point intp, float nl_dot)
+{
+	t_colorf	color, img_col;
+
+	SET_COLOR(color, 0.0f, 0.0f, 0.0f);
+
+	/* image texture */
+	if (!shape->material.texture.data)
+		return (color);
+
+	img_col = get_img_color(intp, shape, shape->material.texture);
+	color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&img_col);
+
+	return (color);
+}
+
+t_colorf	get_diffuse_reflection_color(t_shape *shape, float nl_dot, t_light *light, t_vector dir_pos2light, t_vector eye_dir)
+{
+	t_colorf	color;
+	t_vector	ref_dir, inv_eye_dir, normal;
+	float		vr_dot, vr_dot_pow;
+
+	SET_COLOR(color, 0.0f, 0.0f, 0.0f);
+	inv_eye_dir = normalize_vec_inv(&eye_dir);
+
+	if (light->type == LT_SPOT)
+	{
+		t_vector	light_to_pos = normalize_vec_inv(&dir_pos2light);
+		float		alpha = acosf(dot(&light_to_pos, &light->direction));
+
+		if (alpha > light->angle / 2.0f * (float)M_PI / 180.0f)
+			return (color);
+
+		color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&light->illuminance);
+
+		/* 鏡面反射光 specular を計算してcolに足し合わせる */
+		if (nl_dot <= 0.0f)
+			return (color);
+
+		/* 正反射ベクトルの計算 */
+		ref_dir = vec_calc(2.0f * nl_dot, &normal, -1.0f, &dir_pos2light);
+		normalize(&ref_dir);
+
+		vr_dot = CLAMP(dot(&inv_eye_dir, &ref_dir), 0, 1);
+		vr_dot_pow = powf(vr_dot, shape->material.shininess);
+
+		/* 鏡面反射光の計算 */
+		color = colorf_mul(&color, 1.0f, &shape->material.specular_ref, vr_dot_pow, &light->illuminance);
+		return (color);
+	}
+
+	color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&light->illuminance);
+
+	/* 鏡面反射光 specular を計算してcolに足し合わせる */
+	if (nl_dot <= 0.0f)
+		return (color);
+
+	/* 正反射ベクトルの計算 */
+	ref_dir = vec_calc(2.0f * nl_dot, &normal, -1.0f, &dir_pos2light);
+	normalize(&ref_dir);
+
+	vr_dot = CLAMP(dot(&inv_eye_dir, &ref_dir), 0, 1);
+	vr_dot_pow = powf(vr_dot, shape->material.shininess);
+
+	/* 鏡面反射光の計算 */
+	color = colorf_mul(&color, 1.0f, &shape->material.specular_ref, vr_dot_pow, &light->illuminance);
+	return (color);
+}
+
 t_colorf calc_diffuse_reflection(const t_scene *scene, const t_ray *eye_ray,
 						  t_intersection_point intp, t_shape *shape)
 {
-	t_colorf	color, checker_col, img_col;
-	size_t		i;
+	t_colorf	color;
+	size_t		idx;
 	t_light		*light;
-	t_vector	ref_dir, dir_pos2light, inv_eye_dir, normal;
-	float		nl_dot, vr_dot, vr_dot_pow;
+	t_vector	dir_pos2light, normal;
+	float		nl_dot;
+	t_colorf	color_checker_texture, color_image_texture, color_diffuse_ref;
 
 	SET_COLOR(color, 0.0f, 0.0f, 0.0f);
 
@@ -60,13 +145,11 @@ t_colorf calc_diffuse_reflection(const t_scene *scene, const t_ray *eye_ray,
 	if (shape->material.bump.data)
 		normal = get_bump_normal(intp, shape);
 
-	inv_eye_dir = normalize_vec_inv(&eye_ray->direction);
-
-	i = 0;
-	while (i < scene->num_lights)
+	idx = 0;
+	while (idx < scene->num_lights)
 	{
-		light = &scene->lights[i];
-		i++;
+		light = &scene->lights[idx];
+		idx++;
 
 		dir_pos2light = get_dir_pos2light(*light, intp.position);
 		nl_dot = CLAMP(dot(&normal, &dir_pos2light), 0, 1);
@@ -74,60 +157,12 @@ t_colorf calc_diffuse_reflection(const t_scene *scene, const t_ray *eye_ray,
 		if (is_obj_exists_between_light_and_eye(scene, dir_pos2light, light, intp))
 			continue ;
 
-		/* checker */
-		if (shape->material.is_checker)
-		{
-			checker_col = get_checker_color(intp, shape);
-			color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&checker_col);
-		}
-
-		/* image texture */
-		if (shape->material.texture.data)
-		{
-			img_col = get_img_color(intp, shape, shape->material.texture);
-			color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&img_col);
-		}
-
-		if (light->type == LT_SPOT)
-		{
-			t_vector	light_to_pos = normalize_vec_inv(&dir_pos2light);
-			float		alpha = acosf(dot(&light_to_pos, &light->direction));
-			if (alpha > light->angle / 2.0f * (float)M_PI / 180.0f)
-				continue ;
-
-			color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&light->illuminance);
-
-			if (nl_dot <= 0.0f)
-				continue ;
-
-			/* 正反射ベクトルの計算 */
-			ref_dir = vec_calc(2.0f * nl_dot, &normal, -1.0f, &dir_pos2light);
-			normalize(&ref_dir);
-
-			vr_dot = CLAMP(dot(&inv_eye_dir, &ref_dir), 0, 1);
-			vr_dot_pow = powf(vr_dot, shape->material.shininess);
-
-			/* 鏡面反射光の計算 */
-			color = colorf_mul(&color, 1.0f, &shape->material.specular_ref, vr_dot_pow, &light->illuminance);
-		}
-		else
-		{
-			color = colorf_mul(&color, 1.0f, &shape->material.diffuse_ref, nl_dot,&light->illuminance);
-
-			/* 鏡面反射光 specular を計算してcolに足し合わせる */
-			if (nl_dot <= 0.0f)
-				continue ;
-
-			/* 正反射ベクトルの計算 */
-			ref_dir = vec_calc(2.0f * nl_dot, &normal, -1.0f, &dir_pos2light);
-			normalize(&ref_dir);
-
-			vr_dot = CLAMP(dot(&inv_eye_dir, &ref_dir), 0, 1);
-			vr_dot_pow = powf(vr_dot, shape->material.shininess);
-
-			/* 鏡面反射光の計算 */
-			color = colorf_mul(&color, 1.0f, &shape->material.specular_ref, vr_dot_pow, &light->illuminance);
-		}
+		color_checker_texture = get_checker_reflection_color(shape, intp, nl_dot);
+		color_image_texture = get_image_reflection_color(shape, intp, nl_dot);
+		color_diffuse_ref = get_diffuse_reflection_color(shape, nl_dot, light, dir_pos2light, eye_ray->direction);
+		color = colorf_add(color, color_checker_texture);
+		color = colorf_add(color, color_image_texture);
+		color = colorf_add(color, color_diffuse_ref);
 	}
 	return (color);
 }
