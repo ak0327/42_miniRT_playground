531f5cf405b3111f1d04a701e1442e7d560d9eb6
takira <ca.fie.0220@gmail.com>
2023-04-24 18:54:05 +0900

    #10 add ft_strtod

--- playground/ft_strtod.c
+++ playground/ft_strtod.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:12:48 by takira            #+#    #+#             */
-/*   Updated: 2023/04/12 17:57:29 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/24 18:51:27 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,43 @@
 #include <math.h>
 #include <ctype.h>
 
-double	ft_strtod(const char *str, size_t *err_idx)
+double	ft_strtod(const char *str, bool *is_success)
 {
 	double	ret_num;
-	int 	sign;
+	double 	scale;
+	double 	sign;
 	size_t	idx;
 
-	ret_num = 0;
 	idx = 0;
 	sign = 1;
-
+	*is_success = false;
 	if (!str)
 		return (0);
 
+	ret_num = 0.0;
 	while (isspace(str[idx]))
 		idx++;
-	if (str[idx] == '-' || str[idx] == '+')
-	{
-		if (str[idx] == '-')
+	if (str[idx] == '-')
 			sign = -1;
+	if (str[idx] == '-' || str[idx] == '+')
+		idx++;
+	while ('0' <= str[idx] && str[idx] <= '9')
+	{
+		ret_num = ret_num * 10 + str[idx] - '0';
 		idx++;
 	}
-	if (!isnumber(str[idx]))
-		return (0);
-
-	while (isnumber(str[idx]))
+	if (str[idx] == '.')
+		idx++;
+	scale = 10.0;
+	while ('0' <= str[idx] && str[idx] <= '9')
 	{
-
+		ret_num += (double)(str[idx] - '0') / scale;
+		scale *= 10.0;
+		idx++;
 	}
-
-	return (ret_num);
+	if (!str[idx])
+		*is_success = true;
+	return (sign * ret_num);
 }
 
 int test(const char *str, int no)
@@ -55,39 +62,153 @@ int test(const char *str, int no)
 	double	ft_ret, lib_ret;
 	bool	ft_is_success;
 	char	*lib_err = NULL;
-	int		ng;
+	bool	ok;
 
 	ft_ret = ft_strtod(str, &ft_is_success);
 	lib_ret = strtod(str, &lib_err);
 
-	ng  = 0;
+	ok = false;
 	if (ft_ret == lib_ret)
-		ng = 1;
+//	if (ft_is_success && !lib_err)
+		ok = true;
 
-	printf("[%02d]  ft=%10lf, lib=%lf, ret=%s\n", no, ft_ret, lib_ret, ng ? "OK" : "WA");
-	return (ng);
+	printf("[%02d] s=%55s,  ft=% e,  lib=% e,  ret=%s\n", no, str, ft_ret, lib_ret, ok ? "\x1b[32mAC\x1b[0m" : "\x1b[31mWA\x1b[0m");
+	if (ok)
+		return (1);
+	return (0);
 }
 
 int main(void)
 {
-	int	ng;
+	int	ok;
 	int test_no;
 
 	printf("############ TEST ############\n");
 
-	ng = 0;
-	test_no = 1;
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
-	ng += test("", test_no++);
+	ok = 0;
+	test_no = 0;
+	ok += test("1", ++test_no);
+	ok += test("2147483647", ++test_no);
+	ok += test("-2147483648", ++test_no);
+	ok += test("9223372036854775807", ++test_no);
+	ok += test("-9223372036854775808", ++test_no);
+	ok += test("1.0", ++test_no);
+	ok += test("-1.0", ++test_no);
+	ok += test("0.1", ++test_no);
+	ok += test("0.01", ++test_no);
+	ok += test("0.001", ++test_no);
+	ok += test("0.0001", ++test_no);
+	ok += test("0.00001", ++test_no);
+	ok += test("0.000001", ++test_no);
+	ok += test("0.0000001", ++test_no);
+	ok += test("0.00000001", ++test_no);
+	ok += test("0.000000001", ++test_no);
+	ok += test("0.0000000001", ++test_no);
+	ok += test("0.00000000001", ++test_no);
+	ok += test("0.000000000001", ++test_no);
+	ok += test("0.0000000000001", ++test_no);
+	ok += test("0.00000000000001", ++test_no);
+	ok += test("0.000000000000001", ++test_no);
+	ok += test("0.0000000000000001", ++test_no);
+	ok += test("0.00000000000000001", ++test_no);
+	ok += test("0.000000000000000001", ++test_no);
+	ok += test("0.0000000000000000001", ++test_no);
+	ok += test("0.00000000000000000001", ++test_no);
+	ok += test("0.000000000000000000001", ++test_no);
+	ok += test("0.0000000000000000000001", ++test_no);
+	ok += test("0.00000000000000000000001", ++test_no);
+	ok += test("0.000000000000000000000001", ++test_no);
+	ok += test("0.0000000000000000000000001", ++test_no);
+	ok += test("0.00000000000000000000000001", ++test_no);
+	ok += test("0.000000000000000000000000001", ++test_no);
+	ok += test("0.0000000000000000000000000001", ++test_no);
+	ok += test("1.0000000000000000000000000001", ++test_no);
+	ok += test("10.0000000000000000000000000001", ++test_no);
+	ok += test("100.0000000000000000000000000001", ++test_no);
+	ok += test("1000.0000000000000000000000000001", ++test_no);
+	ok += test("10000.0000000000000000000000000001", ++test_no);
+	ok += test("100000.0000000000000000000000000001", ++test_no);
+	ok += test("1000000.0000000000000000000000000001", ++test_no);
+	ok += test("10000000.0000000000000000000000000001", ++test_no);
+	ok += test("100000000.0000000000000000000000000001", ++test_no);
+	ok += test("1000000000.0000000000000000000000000001", ++test_no);
+	ok += test("10000000000.0000000000000000000000000001", ++test_no);
+	ok += test("100000000000.0000000000000000000000000001", ++test_no);
+	ok += test("1000000000000.0000000000000000000000000001", ++test_no);
+	ok += test("10000000000000.0000000000000000000000000001", ++test_no);
+	ok += test("100000000000000.0000000000000000000000000001", ++test_no);
+	ok += test("0.00000000000000000019", ++test_no);
+	ok += test("0.1001001001001001", ++test_no);
+	ok += test("-1.0000001", ++test_no);
+	ok += test("-1.00000001", ++test_no);
+	ok += test("-1.000000001", ++test_no);
+	ok += test("1.00000001", ++test_no);
+	ok += test("1.000000001", ++test_no);
+	ok += test("1.0000000001", ++test_no);
+	ok += test("1.00000000010000000000", ++test_no);
+
+	ok += test("3.14", ++test_no);
+	ok += test("3.141", ++test_no);
+	ok += test("3.1415", ++test_no);
+	ok += test("3.14159", ++test_no);
+	ok += test("3.141592", ++test_no);
+	ok += test("3.1415926", ++test_no);
+	ok += test("3.14159265", ++test_no);
+	ok += test("3.141592653", ++test_no);
+	ok += test("3.1415926535", ++test_no);
+	ok += test("3.14159265358", ++test_no);
+	ok += test("3.141592653589", ++test_no);
+	ok += test("3.1415926535897", ++test_no);
+	ok += test("3.14159265358979", ++test_no);
+	ok += test("3.141592653589793", ++test_no);
+	ok += test("3.1415926535897932", ++test_no);
+	ok += test("3.14159265358979323", ++test_no);
+	ok += test("3.141592653589793238", ++test_no);
+	ok += test("3.1415926535897932384", ++test_no);
+	ok += test("3.14159265358979323846", ++test_no);
+	ok += test("3.141592653589793238462", ++test_no);
+	ok += test("3.1415926535897932384626", ++test_no);
+	ok += test("3.14159265358979323846264", ++test_no);
+	ok += test("3.141592653589793238462643", ++test_no);
+	ok += test("3.1415926535897932384626433", ++test_no);
+	ok += test("3.14159265358979323846264338", ++test_no);
+	ok += test("3.141592653589793238462643383", ++test_no);
+	ok += test("3.1415926535897932384626433832", ++test_no);
+	ok += test("3.14159265358979323846264338327", ++test_no);
+	ok += test("3.141592653589793238462643383279", ++test_no);
+	ok += test("3.1415926535897932384626433832795", ++test_no);
+	ok += test("3.14159265358979323846264338327950", ++test_no);
+	ok += test("3.141592653589793238462643383279502", ++test_no);
+	ok += test("3.1415926535897932384626433832795028", ++test_no);
+	ok += test("3.14159265358979323846264338327950288", ++test_no);
+	ok += test("3.141592653589793238462643383279502884", ++test_no);
+	ok += test("3.1415926535897932384626433832795028841", ++test_no);
+	ok += test("3.14159265358979323846264338327950288419", ++test_no);
+	ok += test("3.141592653589793238462643383279502884197", ++test_no);
+	ok += test("3.1415926535897932384626433832795028841971", ++test_no);
+	ok += test("3.14159265358979323846264338327950288419716", ++test_no);
+	ok += test("3.141592653589793238462643383279502884197169", ++test_no);
+	ok += test("3.1415926535897932384626433832795028841971693", ++test_no);
+	ok += test("3.14159265358979323846264338327950288419716939", ++test_no);
+	ok += test("3.141592653589793238462643383279502884197169399", ++test_no);
+	ok += test("3.1415926535897932384626433832795028841971693993", ++test_no);
+	ok += test("3.14159265358979323846264338327950288419716939937", ++test_no);
+	ok += test("3.141592653589793238462643383279502884197169399375", ++test_no);
+	ok += test(".1415", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
+	ok += test("", ++test_no);
 
 	printf(" ## RESULT ##\n");
-	printf("     OK: %d, ng: %d\n\n", test_no - ng, ng);
+	printf("     OK: %d, NG: %d\n\n", ok, test_no - ok);
 }
--- srcs/ft_strtod.c
+++ srcs/ft_strtod.c
@@ -6,17 +6,53 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:38:53 by takira            #+#    #+#             */
-/*   Updated: 2023/04/11 13:46:23 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/24 18:53:29 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+// NG : --0.0
+//    :    .123
+//    :   1..23
+//    :   1.23-
 double	ft_strtod(const char *str, bool *is_success)
 {
-	double	num;
+	double	ret_num;
+	double 	scale;
+	double 	sign;
+	size_t	idx;
 
-	num = strtod(str, NULL);
-	*is_success = true;
-return (num);
+	idx = 0;
+	sign = 1;
+	*is_success = false;
+	if (!str)
+		return (0);
+
+	ret_num = 0.0;
+	while (ft_isspace(str[idx]))
+		idx++;
+	if (str[idx] == '-')
+		sign = -1;
+	if (str[idx] == '-' || str[idx] == '+')
+		idx++;
+	if (!ft_isdigit(str[idx]))
+		return (0);
+	while (ft_isdigit(str[idx]))
+	{
+		ret_num = ret_num * 10 + str[idx] - '0';
+		idx++;
+	}
+	if (str[idx] == '.')
+		idx++;
+	scale = 10.0;
+	while (ft_isdigit(str[idx]))
+	{
+		ret_num += (double)(str[idx] - '0') / scale;
+		scale *= 10.0;
+		idx++;
+	}
+	if (!str[idx])
+		*is_success = true;
+	return (sign * ret_num);
 }
