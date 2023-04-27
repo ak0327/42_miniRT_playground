16caa51a2da5aee5c699ec00815c23b7f6b928b2
takira <ca.fie.0220@gmail.com>
2023-04-27 14:55:53 +0900

    #20 push

--- playground/ft_strtod.c
+++ playground/ft_strtod.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:12:48 by takira            #+#    #+#             */
-/*   Updated: 2023/04/27 12:51:38 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/27 14:53:08 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 //	double	scale;
 //	size_t 	i;
 //
-//	i = 0;
 //	while (isspace(str[*idx]))
 //		*idx += 1;
 //	if (str[*idx] == '-')
@@ -76,7 +75,7 @@
 //	return (SUCCESS);
 //}
 //
-//double	ft_strtod(const char *str, bool *is_success, char **err)
+//double	ft_strtod(const char *str, char **err)
 //{
 //	double	num;
 //	double 	num_sign;
@@ -85,7 +84,6 @@
 //	idx = 0;
 //	num = 0.0;
 //	num_sign = 1.0;
-//	*is_success = false;
 //	if (!str)
 //	{
 //		*err = (char *)&str[idx];
@@ -101,95 +99,19 @@
 //		*err = (char *)&str[idx];
 //		return (num_sign * num);
 //	}
-//	if (!str[idx])
-//		*is_success = true;
 //	*err = (char *)&str[idx];
 //	return (num_sign * num);
 //}
 
 
 
-/* IEEE754準拠 */
 
-typedef struct s_prep_number
+
+static int get_num_part(const char *str, double *num, double *sign, size_t *idx, long *exp)
 {
-	int 		sign;
-	int32_t 	exponent;
-	uint64_t	mantissa;
-}				t_prep_number;
+	size_t 	i;
+	int 	digit;
 
-//int	parsing_integer_part(const char *str, size_t *idx, t_prep_number *prep_num)
-//{
-//	*idx = 0;
-//	while (isspace(str[*idx]))
-//		*idx += 1;
-//	if (!str[*idx])
-//		return (FAILURE);
-//	if (str[*idx] == '-')
-//		prep_num->sign = -1;
-//	if (str[*idx] == '-' || str[*idx] == '+')
-//		*idx += 1;
-//	while (isdigit(str[*idx]))
-//	{
-//		prep_num->mantissa = prep_num->mantissa * 10 + str[*idx] - '0';
-//		*idx += 1;
-//	}
-//	if (!str[*idx])
-//		return (SUCCESS);
-//	return (SUCCESS);
-//}
-//
-//int	parsing_fractional_part(const char *str, size_t *idx, t_prep_number *prep_num)
-//{
-//	if (str[*idx] == '.')
-//		*idx += 1;
-//	if (!(!str[*idx] || isdigit(str[*idx]) || str[*idx] == 'e' || str[*idx] == 'E'))
-//		return (FAILURE);
-//	while (isdigit(str[*idx]))
-//	{
-//		prep_num->mantissa = prep_num->mantissa * 10 + str[*idx] - '0';
-//		prep_num->exponent--;
-//		*idx += 1;
-//	}
-//	return (SUCCESS);
-//}
-//
-//int	parsing_exponential_part(const char *str, size_t *idx, t_prep_number *prep_num)
-//{
-//	int		sign;
-//	int32_t	exp;
-//
-//	sign = 1;
-//	exp = 0;
-//	if (!(str[*idx] == 'e' || str[*idx] == 'E'))
-//		return (SUCCESS);
-//	if (str[*idx] == '-')
-//		sign = -1;
-//	if (str[*idx] == '+' || str[*idx] == '-')
-//		*idx += 1;
-//	if (!str[*idx] || !isdigit(str[*idx]))
-//		return (FAILURE);
-//	while (isdigit(str[*idx]))
-//	{
-//		exp = exp * 10 + str[*idx] - '0';
-//		*idx += 1;
-//	}
-//	prep_num->exponent += sign * exp;
-//	if (str[*idx])
-//		return (FAILURE);
-//	return (SUCCESS);
-//}
-//
-
-
-
-
-static int get_num_part(const char *str, uint64_t *n, double *sign, size_t *idx, long *exp)
-{
-	size_t 		i;
-	int 		digit;
-
-	*n = 0;
 	*exp = 0;
 	while (isspace(str[*idx]))
 		*idx += 1;
@@ -201,92 +123,173 @@ static int get_num_part(const char *str, uint64_t *n, double *sign, size_t *idx,
 	if (!str[*idx + i] || !(isdigit(str[*idx + i]) || (str[*idx + i] == '.' && isdigit(str[*idx + i + 1]))))
 		return (FAILURE);
 	*idx += i;
-	while (isdigit(str[*idx]))
+	while ('0' <= str[*idx] && str[*idx] <= '9')
 	{
 		digit = str[*idx] - '0';
-		if (*n <= SIZE_MAX / 10)
-			*n = *n * 10 + digit;
+		if (*num * 10 + digit >= *num)
+			*num = *num * 10 + str[*idx] - '0';
 		else
 			*exp += 1;
 		*idx += 1;
 	}
-//	printf("[get_num_part-1] n:%llo, exp:%ld\n", *n, *exp);
 	if (str[*idx] == '.')
 	{
 		*idx += 1;
-		while (isdigit(str[*idx]))
+		while ('0' <= str[*idx] && str[*idx] <= '9')
 		{
-			if (*n <= DBL_MAX / 10)
-				*n = *n * 10 + str[*idx] - '0';
+			digit = str[*idx] - '0';
+			if (*num * 10 + digit >= *num)
+				*num = *num * 10 + digit;
 			else
 				*exp += 1;
 			*exp -= 1;
 			*idx += 1;
 		}
 	}
-//	printf("[get_num_part-2] n:%llo, exp:%ld\n", *n, *exp);
 	return (SUCCESS);
 }
 
-static int	get_exp_part(const char *str, size_t *idx, long *exp)
+static bool	is_under_long(const long num, int digit, bool negative)
 {
-	bool	exp_negative;
-	long 	exp_val;
-	size_t 	i;
+	long of_div = LONG_MAX / 10;
+	long of_mod = LONG_MAX % 10;
+	if (negative)
+	{
+		of_div = -(LONG_MIN / 10);
+		of_mod = -(LONG_MIN % 10);
+	}
 
-	exp_negative = false;
+	if (num > of_div)
+		return (false);
+	if (num == of_div && digit > of_mod)
+		return (false);
+	return (true);
+}
+
+static int	get_exp_part(const char *str, double *num, size_t *idx, long *e)
+{
+	double 	negative;
+	size_t 	i;
+	long 	exp;
+	int 	digit;
+
+	exp = 0;
+	negative = false;
 	if (!str[*idx] || !(str[*idx] == 'e' || str[*idx] == 'E'))
 		return (SUCCESS);
 	i = 1;
 	if (!str[*idx + i])
 		return (FAILURE);
 	if (str[*idx + 1] == '-')
-		exp_negative = true;
+		negative = true;
 	if (str[*idx + i] == '-' || str[*idx + i] == '+')
 		i++;
 	if (!str[*idx + i] || !isdigit(str[*idx + i]))
 		return (FAILURE);
 	while ('0' <= str[*idx + i] && str[*idx + i] <= '9')
 	{
-		if (exp_negative && exp_val < LONG_MIN / 10)
-		{
-			exp_val = LONG_MIN;
-			break ;
-		}
-		else if (!exp_negative && exp_val > LONG_MAX / 10)
-		{
-			exp_val = LONG_MAX;
-			break ;
-		}
-		if (exp_negative)
-			exp_val = exp_val * 10 - (str[*idx + i] - '0');
+		digit = str[*idx + i] - '0';
+		if (is_under_long(exp, digit, negative))
+			exp = exp * 10 + digit;
 		else
-			exp_val = exp_val * 10 + (str[*idx + i] - '0');
+		{
+			if (negative)
+				exp = LONG_MIN;
+			else
+				exp = LONG_MAX;
+			break ;
+		}
 		i++;
 	}
-	*exp = *exp + exp_val;
-//	printf("[get_exp_part] exp:%ld, exp_val:%ld\n", *exp, exp_val);
+	if (exp != LONG_MIN && negative)
+		exp = -exp;
+
+	if ((exp >= 0 && *e <= 0) || (exp <= 0 && *e >= 0))
+		*e += exp;
+	else if (exp >= 0 && *e >= 0)
+	{
+		if (LONG_MAX - exp >= *e)
+			*e += exp;
+		else
+			*e = LONG_MAX;
+	}
+	else
+	{
+		if (exp - LONG_MIN >= -*e)
+			*e += exp;
+		else
+			*e = LONG_MIN;
+	}
+
+	*num *= pow(10, *e);
 	*idx += i;
 	return (SUCCESS);
 }
 
-double	get_double_fpn(uint64_t n, long exp)
-{
-	double	num;
-
-	num = n * pow(10, exp);
-
-//	printf("[get_fpn]n:%lld, exp:%ld, num:%f\n", n, exp, num);
-	return (num);
-}
+//static int	get_exp_part(const char *str, double *num, size_t *idx, long *e)
+//{
+//	double 	negative;
+//	size_t 	i;
+//	long 	exp;
+//	int 	digit;
+//
+//	exp = 0;
+//	negative = false;
+//	if (!str[*idx] || !(str[*idx] == 'e' || str[*idx] == 'E'))
+//		return (SUCCESS);
+//	i = 1;
+//	if (!str[*idx + i])
+//		return (FAILURE);
+//	if (str[*idx + 1] == '-')
+//		negative = true;
+//	if (str[*idx + i] == '-' || str[*idx + i] == '+')
+//		i++;
+//	if (!str[*idx + i] || !isdigit(str[*idx + i]))
+//		return (FAILURE);
+//	while ('0' <= str[*idx + i] && str[*idx + i] <= '9')
+//	{
+//		digit = str[*idx + i] - '0';
+//		if (is_under_long(exp, digit, negative))
+//			exp = exp * 10 + digit;
+//		else
+//		{
+//			if (negative)
+//				exp = LONG_MIN;
+//			else
+//				exp = LONG_MAX;
+//			break ;
+//		}
+//		i++;
+//	}
+//	if (exp != LONG_MIN && negative)
+//		exp = -exp;
+//	if ((exp >= 0 && *e <= 0) || (exp <= 0 && *e >= 0))
+//		*e += exp;
+//	else if (exp >= 0 && *e >= 0)
+//	{
+//		if (LONG_MAX - exp >= *e)
+//			*e += exp;
+//		else
+//			*e = LONG_MAX;
+//	}
+//	else
+//	{
+//		if (exp - LONG_MIN >= -*e)
+//			*e += exp;
+//		else
+//			*e = LONG_MIN;
+//	}
+//
+//	*num *= pow(10, exp);
+//	*idx += i;
+//	return (SUCCESS);
+//}
 
 double	ft_strtod(const char *str, char **err)
 {
-	uint64_t	n;
-	long 		exp;
-
 	double	num;
 	double 	num_sign;
+	long 	exp;
 	size_t	idx;
 
 	idx = 0;
@@ -298,20 +301,155 @@ double	ft_strtod(const char *str, char **err)
 		*err = (char *)&str[idx];
 		return (num);
 	}
-	if (get_num_part(str, &n, &num_sign, &idx, &exp) == FAILURE)
+	if (get_num_part(str, &num, &num_sign, &idx, &exp) == FAILURE)
 	{
 		*err = (char *)&str[idx];
 		return (num);
 	}
-	if (get_exp_part(str,&idx, &exp) == FAILURE)
+	if (get_exp_part(str, &num, &idx, &exp) == FAILURE)
 	{
 		*err = (char *)&str[idx];
 		return (num_sign * num);
 	}
-
-	num = get_double_fpn(n, exp);
-
 	*err = (char *)&str[idx];
 	return (num_sign * num);
 }
 
+
+
+
+
+
+
+//
+//static int get_num_part(const char *str, uint64_t *n, double *sign, size_t *idx, long *exp)
+//{
+//	size_t 		i;
+//	int 		digit;
+//
+//	*n = 0;
+//	*exp = 0;
+//	while (isspace(str[*idx]))
+//		*idx += 1;
+//	if (str[*idx] == '-')
+//		*sign = -1.0;
+//	i = 0;
+//	if (str[*idx + i] == '-' || str[*idx + i] == '+')
+//		i++;
+//	if (!str[*idx + i] || !(isdigit(str[*idx + i]) || (str[*idx + i] == '.' && isdigit(str[*idx + i + 1]))))
+//		return (FAILURE);
+//	*idx += i;
+//	while (isdigit(str[*idx]))
+//	{
+//		digit = str[*idx] - '0';
+//		if (*n <= SIZE_MAX / 10)
+//			*n = *n * 10 + digit;
+//		else
+//			*exp += 1;
+//		*idx += 1;
+//	}
+////	printf("[get_num_part-1] n:%llo, exp:%ld\n", *n, *exp);
+//	if (str[*idx] == '.')
+//	{
+//		*idx += 1;
+//		while (isdigit(str[*idx]))
+//		{
+//			if (*n <= DBL_MAX / 10)
+//				*n = *n * 10 + str[*idx] - '0';
+//			else
+//				*exp += 1;
+//			*exp -= 1;
+//			*idx += 1;
+//		}
+//	}
+////	printf("[get_num_part-2] n:%llo, exp:%ld\n", *n, *exp);
+//	return (SUCCESS);
+//}
+//
+//static int	get_exp_part(const char *str, size_t *idx, long *exp)
+//{
+//	bool	exp_negative;
+//	long 	exp_val;
+//	size_t 	i;
+//
+//	exp_negative = false;
+//	if (!str[*idx] || !(str[*idx] == 'e' || str[*idx] == 'E'))
+//		return (SUCCESS);
+//	i = 1;
+//	if (!str[*idx + i])
+//		return (FAILURE);
+//	if (str[*idx + 1] == '-')
+//		exp_negative = true;
+//	if (str[*idx + i] == '-' || str[*idx + i] == '+')
+//		i++;
+//	if (!str[*idx + i] || !isdigit(str[*idx + i]))
+//		return (FAILURE);
+//	while ('0' <= str[*idx + i] && str[*idx + i] <= '9')
+//	{
+//		if (exp_negative && exp_val < LONG_MIN / 10)
+//		{
+//			exp_val = LONG_MIN;
+//			break ;
+//		}
+//		else if (!exp_negative && exp_val > LONG_MAX / 10)
+//		{
+//			exp_val = LONG_MAX;
+//			break ;
+//		}
+//		if (exp_negative)
+//			exp_val = exp_val * 10 - (str[*idx + i] - '0');
+//		else
+//			exp_val = exp_val * 10 + (str[*idx + i] - '0');
+//		i++;
+//	}
+//	*exp = *exp + exp_val;
+////	printf("[get_exp_part] exp:%ld, exp_val:%ld\n", *exp, exp_val);
+//	*idx += i;
+//	return (SUCCESS);
+//}
+//
+//double	get_double_fpn(uint64_t n, long exp)
+//{
+//	double	num;
+//
+//	num = n * pow(10, exp);
+//
+////	printf("[get_fpn]n:%lld, exp:%ld, num:%f\n", n, exp, num);
+//	return (num);
+//}
+//
+//double	ft_strtod(const char *str, char **endptr)
+//{
+//	uint64_t	n;
+//	long 		exp;
+//
+//	double	num;
+//	double 	num_sign;
+//	size_t	idx;
+//
+//	idx = 0;
+//	num = 0.0;
+//	num_sign = 1.0;
+//	exp = 0;
+//	if (!str)
+//	{
+//		*endptr = (char *)&str[idx];
+//		return (num);
+//	}
+//	if (get_num_part(str, &n, &num_sign, &idx, &exp) == FAILURE)
+//	{
+//		*endptr = (char *)&str[idx];
+//		return (num);
+//	}
+//	if (get_exp_part(str,&idx, &exp) == FAILURE)
+//	{
+//		*endptr = (char *)&str[idx];
+//		return (num_sign * num);
+//	}
+//
+//	num = get_double_fpn(n, exp);
+//
+//	*endptr = (char *)&str[idx];
+//	return (num_sign * num);
+//}
+//
--- playground/test.c
+++ playground/test.c
@@ -6,27 +6,44 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 18:56:00 by takira            #+#    #+#             */
-/*   Updated: 2023/04/27 13:10:55 by takira           ###   ########.fr       */
+/*   Updated: 2023/04/27 14:36:13 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-typedef union longlongdouble{
-	long long llnum;
-	double dnum;
-}			longlongdouble;
-
 int main(void)
 {
-	longlongdouble lldnum = {0};
-	lldnum.dnum = 1.2345;
-	printf("llnum:%lld, dnum:%f\n", lldnum.llnum, lldnum.dnum);
+	printf("LONG_MAX   : % ld\n", LONG_MAX);
+	printf("LONG_MAX/10: % ld\n", LONG_MAX / 10);
+	printf("LONG_MAX%%10: % ld\n", LONG_MAX % 10);
+	printf("\n");
+	printf("LONG_MIN   : % ld\n", LONG_MIN);
+	printf("LONG_MIN/10: % ld\n", LONG_MIN / 10);
+	printf("LONG_MIN%%10: % ld\n", LONG_MIN % 10);
+	printf("\n");
+	printf("-(LONG_MIN/10): % ld\n", -(LONG_MIN / 10));
+	printf("-LONG_MIN/10  : % ld\n", -LONG_MIN / 10);
 
-	printf("fnum:%016llx\n", lldnum.llnum);
-	lldnum.llnum = lldnum.llnum >> 8;
-	printf("fnum:%016llx\n", lldnum.llnum);
-	lldnum.llnum &= 0xFFFFFFFF00000000;
-	printf("fnum:%016llx\n", lldnum.llnum);
-	return 0;
+	printf("-(LONG_MIN%%10): % ld\n", -(LONG_MIN % 10));
+	printf("-LONG_MIN%%10  : % ld\n", -LONG_MIN % 10);
+
+	return (0);
 }
+
+/*
+
+LONG_MAX   : 9223372036854775807
+LONG_MAX/10: 922337203685477580
+LONG_MAX%10: 7
+
+LONG_MIN   : -9223372036854775808
+LONG_MIN/10: -922337203685477580
+LONG_MIN%10: -8
+
+-(LONG_MIN/10):  922337203685477580
+-LONG_MIN/10  : -922337203685477580
+-(LONG_MIN%10):  8
+-LONG_MIN%10  : -8
+
+ */
