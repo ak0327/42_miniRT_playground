7ad039a62da37a338f7541cfc018c15313e6ef32
takira <ca.fie.0220@gmail.com>
2023-04-29 22:09:02 +0900

    #23 strtod update未完成版 push, Pend.

--- playground/bignum.c
+++ playground/bignum.c
@@ -1,3 +1,25 @@
-//
-// Created by Akira TANIDA on 2023/04/29.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bignum.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 10:51:29 by takira            #+#    #+#             */
+/*   Updated: 2023/04/29 11:45:50 by takira           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+#define	SIZE	1024
+
+typedef struct s_bignum
+{
+	int		num[SIZE];
+	bool	negative;
+	size_t	len;
+	size_t	max_len;
+
+}	t_bignum;
+
